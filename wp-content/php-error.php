8b1b21676f85dbfc2ddafbb06c8faf93dab697f6
Chris Galbraith <chris@resknow.co.uk>
2021-12-23 12:11:33 +0000

    Add custom PHP error page

---
+++ wp-content/php-error.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Something went wrong</title>
+    <style>
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            font-size: 1rem;
+        }
+
+        main {
+            max-width: 65ch;
+            margin-inline: auto;
+            padding: 48px 24px;
+        }
+
+        main:after {
+            background-color: #f55b4c;
+            height: 4px;
+            content: "";
+            display: block;
+            margin-block-start: 4em;
+            width: max(40%, 100px);
+        }
+
+        h1 {
+            font-weight: 900;
+            letter-spacing: -.5px;
+            margin-block: 0;
+        }
+
+        p {
+            color: #373946;
+            font-size: 1.2em;
+            margin-block: .6em;
+        }
+    </style>
+</head>
+<body>
+
+        <main>
+            <h1>Something went wrong.</h1>
+            <p>Our team have been notified and we'll get it sorted as soon as we can!</p>
+        </main>
+
+</body>
+</html>
